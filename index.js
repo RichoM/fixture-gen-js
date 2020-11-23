93d9cf58d945bc39023fab9e8349e5529db5bcfd
RichoM <richi.moran@gmail.com>
2020-11-23 20:56:14 -0300

    Fixed index.js

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+function createTeams(n) {
+  const letters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+  let teams = [];
+  for (let i = 0; i < n; i++) {
+    teams.push(letters[i]);
+  }
+  return teams;
+}
+
 rl.question('Cantidad de equipos? ', (answer) => {
   let n = parseInt(answer);
-  let teams = f.createTeams(n);
-  let rounds = f.balance(teams, f.rounds(teams));
+  let teams = createTeams(n);
+  let rounds = f.create(teams);
 
   console.log(`Equipos: ${JSON.stringify(teams)}`);
   rounds.forEach((r, i) => {
