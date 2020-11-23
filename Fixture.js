975278a6b769942c4229a97ce8d3621dea694e7c
RichoM <richi.moran@gmail.com>
2020-11-23 18:56:43 -0300

    Refactoring

--- Fixture.js
+++ Fixture.js
@@ -1,103 +1,65 @@
+var Fixture = (function () {
 
-function createTeams(n) {
-  const letters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-  let teams = [];
-  for (let i = 0; i < n; i++) {
-    teams.push(letters[i]);
-  }
-  return teams;
-}
-
-function matches(teams) {
-  let matches = [];
-  for (let i = 0; i < teams.length - 1; i++) {
-    for (let j = i + 1; j < teams.length; j++) {
-      if ((j - i) % 2 == 0) {
-        matches.push({r: teams[j], g: teams[i]});
-      } else {
-        matches.push({r: teams[i], g: teams[j]});
+  function matches(teams) {
+    let matches = [];
+    for (let i = 0; i < teams.length - 1; i++) {
+      for (let j = i + 1; j < teams.length; j++) {
+        if ((j - i) % 2 == 0) {
+          matches.push({r: teams[j], g: teams[i]});
+        } else {
+          matches.push({r: teams[i], g: teams[j]});
+        }
       }
     }
-  }
-  return matches;
-}
-
-/*
-NOTE(Richo): Cyclic algorithm taken from https://www.devenezia.com/downloads/round-robin/
-*/
-function rounds(t) {
-  let ghost = -1;
-  let teams = t.slice();
-  if (teams.length % 2 == 1) {
-    teams.unshift(ghost);
+    return matches;
   }
 
-  let rounds = [];
-  let fixed = teams[0];
-  let rest = teams.slice(1);
-  for (let i = 0; i < teams.length - 1; i++) {
-    let round = [];
-    rest.unshift(rest.pop()); // Cycle
-    if (fixed != ghost) {
-      round.push({r: fixed, g: rest[0]});
+  /*
+  NOTE(Richo): Cyclic algorithm taken from https://www.devenezia.com/downloads/round-robin/
+  */
+  function rounds(t) {
+    let ghost = -1;
+    let teams = t.slice();
+    if (teams.length % 2 == 1) {
+      teams.unshift(ghost);
     }
-    for (let j = 0; j < (rest.length - 1) / 2; j++) {
-      let r = j + 1;
-      let g = rest.length - (j + 1);
-      let match = {r: rest[r], g: rest[g]};
-      round.push(match);
+
+    let rounds = [];
+    let fixed = teams[0];
+    let rest = teams.slice(1);
+    for (let i = 0; i < teams.length - 1; i++) {
+      let round = [];
+      rest.unshift(rest.pop()); // Cycle
+      if (fixed != ghost) {
+        round.push({r: fixed, g: rest[0]});
+      }
+      for (let j = 0; j < (rest.length - 1) / 2; j++) {
+        let r = j + 1;
+        let g = rest.length - (j + 1);
+        let match = {r: rest[r], g: rest[g]};
+        round.push(match);
+      }
+      rounds.push(round);
     }
-    rounds.push(round);
+    return rounds;
   }
-  return rounds;
-}
 
-function distinctTeams(matches) {
-  matches = matches.flat();
-  let teams = new Set();
-  matches.forEach(m => {
-    teams.add(m.r);
-    teams.add(m.g);
-  });
-  return teams;
-}
+  function balance(teams, rounds) {
+    let balancedMatches = new Set(matches(teams).map(m => JSON.stringify(m)));
+    return rounds.map(round => round.map(match => {
+      if (balancedMatches.has(JSON.stringify(match))) return match;
+      return {r: match.g, g: match.r};
+    }));
+  }
 
-function isBalanced(matches) {
-  matches = matches.flat();
-  let teams = distinctTeams(matches);
-  let half = (teams.size - 1) / 2;
-  let expected = new Set([Math.floor(half), Math.ceil(half)]);
-  return Array.from(teams).every(team => expected.has(matches.filter(m => m.r == team).length));
-}
+  return {
+    create: teams => balance(teams, rounds(teams)),
 
-function balance(teams, rounds) {
-  let balancedMatches = new Set(matches(teams).map(m => JSON.stringify(m)));
-  return rounds.map(round => round.map(match => {
-    if (balancedMatches.has(JSON.stringify(match))) return match;
-    return {r: match.g, g: match.r};
-  }));
-}
+    /* TESTING */
+    matches: matches,
+    rounds: rounds,
+    balance: balance
+  };
+})();
 
-function isComplete(teams, rounds) {
-  let expected = matches(teams);
-  let actual = rounds.flat();
-  if (expected.length != actual.length) return false;
-
-  let expected_set = new Set(expected.map(m => JSON.stringify(m)));
-  let actual_set = new Set(actual.map(m => JSON.stringify(m)));
-  if (actual.length != actual_set.size) return false; // No duplicates!
-
-  if (!expected.every(m => actual_set.has(JSON.stringify(m)))) return false;
-  if (!actual.every(m => expected_set.has(JSON.stringify(m)))) return false;
-
-  return true;
-}
-
-module.exports = {
-  createTeams: createTeams,
-  matches: matches,
-  rounds: rounds,
-  isBalanced: isBalanced,
-  balance: balance,
-  isComplete: isComplete
-};
+module.exports = Fixture;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,48 @@
 var assert = require('assert');
 var f = require("../Fixture.js");
 
+function createTeams(n) {
+  const letters = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+  let teams = [];
+  for (let i = 0; i < n; i++) {
+    teams.push(letters[i]);
+  }
+  return teams;
+}
+
+function distinctTeams(matches) {
+  matches = matches.flat();
+  let teams = new Set();
+  matches.forEach(m => {
+    teams.add(m.r);
+    teams.add(m.g);
+  });
+  return teams;
+}
+
+function isBalanced(matches) {
+  matches = matches.flat();
+  let teams = distinctTeams(matches);
+  let half = (teams.size - 1) / 2;
+  let expected = new Set([Math.floor(half), Math.ceil(half)]);
+  return Array.from(teams).every(team => expected.has(matches.filter(m => m.r == team).length));
+}
+
+function isComplete(teams, rounds) {
+  let expected = f.matches(teams);
+  let actual = rounds.flat();
+  if (expected.length != actual.length) return false;
+
+  let expected_set = new Set(expected.map(m => JSON.stringify(m)));
+  let actual_set = new Set(actual.map(m => JSON.stringify(m)));
+  if (actual.length != actual_set.size) return false; // No duplicates!
+
+  if (!expected.every(m => actual_set.has(JSON.stringify(m)))) return false;
+  if (!actual.every(m => expected_set.has(JSON.stringify(m)))) return false;
+
+  return true;
+}
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
@@ -12,31 +54,31 @@ describe('Array', function() {
 describe("Fixture", function () {
   describe('create-teams', function () {
     it('create empty party', function() {
-      assert.equal(JSON.stringify([]), JSON.stringify(f.createTeams(0)));
+      assert.equal(JSON.stringify([]), JSON.stringify(createTeams(0)));
     });
     it('create party with 3 teams', function() {
-      assert.equal(JSON.stringify(["A", "B", "C"]), JSON.stringify(f.createTeams(3)));
+      assert.equal(JSON.stringify(["A", "B", "C"]), JSON.stringify(createTeams(3)));
     });
   });
 
   describe("create-matches", function () {
     it('create matches with 3 teams', function () {
-      let teams = f.createTeams(3);
+      let teams = createTeams(3);
       let matches = f.matches(teams);
       assert.equal(JSON.stringify(["AB","CA","BC"]), JSON.stringify(matches.map(m => m.r+m.g)));
-      assert.ok(f.isBalanced(matches), "Matches are not balanced!");
+      assert.ok(isBalanced(matches), "Matches are not balanced!");
     });
     it('create matches with 4 teams', function () {
-      let teams = f.createTeams(4);
+      let teams = createTeams(4);
       let matches = f.matches(teams);
       assert.equal(JSON.stringify(["AB","CA","AD","BC","DB","CD"]), JSON.stringify(matches.map(m => m.r+m.g)));
-      assert.ok(f.isBalanced(matches), "Matches are not balanced!");
+      assert.ok(isBalanced(matches), "Matches are not balanced!");
     });
   });
 
   describe("rounds", function () {
     it ("create rounds with 4 teams", function () {
-      let teams = f.createTeams(4);
+      let teams = createTeams(4);
       let actual = f.rounds(teams);
       let expected = [["AD", "BC"],
                       ["AC", "DB"],
@@ -44,7 +86,7 @@ describe("Fixture", function () {
       assert.equal(JSON.stringify(expected), JSON.stringify(actual.map(matches => matches.map(m => m.r+m.g))));
     });
     it ("create rounds with 5 teams", function () {
-      let teams = f.createTeams(5);
+      let teams = createTeams(5);
       let actual = f.rounds(teams);
       let expected = [["AD", "BC"],
                       ["EC", "AB"],
@@ -58,29 +100,29 @@ describe("Fixture", function () {
 
   describe("balanced rounds", function () {
     it ("create balanced rounds with 4 teams", function () {
-      let teams = f.createTeams(4);
+      let teams = createTeams(4);
       let rounds = f.rounds(teams);
       let actual = f.balance(teams, rounds);
-      assert.ok(f.isBalanced(actual), "Matches are not balanced!");
-      assert.ok(f.isComplete(teams, actual), "Not all possible matches!");
+      assert.ok(isBalanced(actual), "Matches are not balanced!");
+      assert.ok(isComplete(teams, actual), "Not all possible matches!");
     });
     it ("create balanced rounds with 5 teams", function () {
-      let teams = f.createTeams(5);
+      let teams = createTeams(5);
       let rounds = f.rounds(teams);
       let actual = f.balance(teams, rounds);
-      assert.ok(f.isBalanced(actual), "Matches are not balanced!");
-      assert.ok(f.isComplete(teams, actual), "Not all possible matches!");
+      assert.ok(isBalanced(actual), "Matches are not balanced!");
+      assert.ok(isComplete(teams, actual), "Not all possible matches!");
     });
   });
 
   describe("full check", function () {
     it("check all rounds from 2 to 25", function () {
       for (let i = 2; i < 26; i++) {
-        let teams = f.createTeams(i);
+        let teams = createTeams(i);
         let rounds = f.rounds(teams);
         let actual = f.balance(teams, rounds);
-        assert.ok(f.isBalanced(actual), "Matches are not balanced!");
-        assert.ok(f.isComplete(teams, actual), "Not all possible matches!");
+        assert.ok(isBalanced(actual), "Matches are not balanced!");
+        assert.ok(isComplete(teams, actual), "Not all possible matches!");
       }
     })
   })
