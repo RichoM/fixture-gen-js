6494628b9a02852a3bbeb5a762f8fcd0d179a11e
RichoM <richi.moran@gmail.com>
2020-11-23 23:25:08 -0300

    Replaced fixture algorithm with code from wikipedia

--- Fixture.js
+++ Fixture.js
@@ -1,62 +1,103 @@
 var Fixture = (function () {
 
-  function matches(teams) {
-    let matches = [];
-    for (let i = 0; i < teams.length - 1; i++) {
-      for (let j = i + 1; j < teams.length; j++) {
-        if ((j - i) % 2 == 0) {
-          matches.push({home: teams[j], away: teams[i]});
-        } else {
-          matches.push({home: teams[i], away: teams[j]});
+  function even(nteams) {
+    let nrounds = nteams - 1;
+    let matches_per_round = nteams / 2;
+
+    let rounds = new Array(nrounds);
+    for (let i = 0; i < rounds.length; i++) {
+      rounds[i] = new Array(matches_per_round);
+    }
+
+    for (let i = 0, k = 0; i < nrounds; i++) {
+      for (let j = 0; j < matches_per_round; j++) {
+        rounds[i][j] = {home: -1, away: -1};
+        rounds[i][j].home = k;
+
+        k++;
+        if (k == nrounds) {
+          k = 0;
         }
       }
     }
-    return matches;
-  }
 
-  /*
-  NOTE(Richo): Cyclic algorithm taken from https://www.devenezia.com/downloads/round-robin/
-  */
-  function rounds(t) {
-    let ghost = -1;
-    let teams = t.slice();
-    if (teams.length % 2 == 1) {
-      teams.unshift(ghost);
+    for (let i = 0; i < nrounds; i++) {
+      if (i % 2 == 0) {
+        rounds[i][0].away = nteams - 1;
+      } else {
+        rounds[i][0].away = rounds[i][0].home;
+        rounds[i][0].home = nteams - 1;
+      }
     }
 
-    let rounds = [];
-    let fixed = teams[0];
-    let rest = teams.slice(1);
-    for (let i = 0; i < teams.length - 1; i++) {
-      let round = [];
-      rest.unshift(rest.pop()); // Cycle
-      if (fixed != ghost) {
-        round.push({home: fixed, away: rest[0]});
+    let last_team = nteams - 1;
+    let last_odd_team = last_team - 1;
+
+    for (let i = 0, k = last_odd_team; i < nrounds; i++) {
+      for (let j = 1; j < matches_per_round; j++) {
+        rounds[i][j].away = k;
+
+        k--;
+
+        if (k == -1) {
+          k = last_odd_team;
+        }
       }
-      for (let j = 0; j < (rest.length - 1) / 2; j++) {
-        let r = j + 1;
-        let g = rest.length - (j + 1);
-        let match = {home: rest[r], away: rest[g]};
-        round.push(match);
-      }
-      rounds.push(round);
     }
+
     return rounds;
   }
 
-  function balance(teams, rounds) {
-    let balancedMatches = new Set(matches(teams).map(m => JSON.stringify(m)));
-    return rounds.map(round => round.map(match => {
-      if (balancedMatches.has(JSON.stringify(match))) return match;
-      return {home: match.away, away: match.home};
-    }));
+  function odd(nteams) {
+    let nrounds = nteams;
+    let matches_per_round = Math.floor(nteams / 2);
+
+    let rounds = new Array(nrounds);
+    for (let i = 0; i < rounds.length; i++) {
+      rounds[i] = new Array(matches_per_round);
+    }
+
+    for (let i = 0, k = 0; i < nrounds; i++) {
+      for (let j = -1; j < matches_per_round; j++) {
+        if (j >= 0) {
+          rounds[i][j] = {home: -1, away: -1};
+          rounds[i][j].home = k;
+        }
+
+        k++;
+
+        if (k == nrounds) {
+          k = 0;
+        }
+      }
+    }
+
+    let last_team = nteams - 1;
+
+    for (let i = 0, k = last_team; i < nrounds; i++) {
+      for (let j = 0; j < matches_per_round; j++) {
+        rounds[i][j].away = k;
+
+        k--;
+
+        if (k == -1) {
+          k = last_team;
+        }
+      }
+    }
+
+    return rounds;
   }
 
   return {
     create: teams => {
-      let indices = teams.map((_, i) => i);
-      let balanced_rounds = balance(indices, rounds(indices));
-      return balanced_rounds.map(round => round.map(match => ({
+      let rounds = null;
+      if (teams.length % 2 == 0) {
+        rounds = even(teams.length);
+      } else {
+        rounds = odd(teams.length);
+      }
+      return rounds.map(round => round.map(match => ({
         home: teams[match.home],
         away: teams[match.away]
       })));
